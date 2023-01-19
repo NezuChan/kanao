3f11778ee5df54b002d5069c468ac29874ecec23
KagChi <yonuxbot@gmail.com>
2023-01-19 19:37:01 +0700

    fix: use void

--- src/Structures/Worker.ts
+++ src/Structures/Worker.ts
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 
-await bootstrapper.bootstrap({
+void bootstrapper.bootstrap({
     forwardEvents: [
         WebSocketShardEvents.Closed,
         WebSocketShardEvents.Debug,
