a3b8d7f4b4beb4019f568d87d0a480361e9dbdb3
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2023-04-04 09:20:36 -0300

    fix: role delete listener

--- src/Listeners/GuildRoleDeleteListener.ts
+++ src/Listeners/GuildRoleDeleteListener.ts
@@ -1,5 +1,5 @@
 /* eslint-disable class-methods-use-this */
-import { GatewayDispatchEvents, GatewayGuildRoleCreateDispatch } from "discord-api-types/v10";
+import { GatewayDispatchEvents, GatewayGuildRoleDeleteDispatch } from "discord-api-types/v10";
 import { Listener, ListenerOptions } from "../Stores/Listener.js";
 import { ApplyOptions } from "../Utilities/Decorators/ApplyOptions.js";
 import { Constants } from "../Utilities/Constants.js";
@@ -10,9 +10,9 @@ import { Constants } from "../Utilities/Constants.js";
 }))
 
 export class GuildRoleDeleteListener extends Listener {
-    public async run(payload: { data: GatewayGuildRoleCreateDispatch }): Promise<void> {
-        await this.container.gateway.redis.srem(process.env.USE_ROUTING === "true" ? `${this.container.gateway.clientId}:${Constants.ROLE_KEY}${Constants.KEYS_SUFFIX}` : `${Constants.ROLE_KEY}${Constants.KEYS_SUFFIX}`, `${payload.data.d.guild_id}:${payload.data.d.role.id}`);
-        await this.container.gateway.cache.roles.delete(`${payload.data.d.guild_id}:${payload.data.d.role.id}`);
+    public async run(payload: { data: GatewayGuildRoleDeleteDispatch }): Promise<void> {
+        await this.container.gateway.redis.srem(process.env.USE_ROUTING === "true" ? `${this.container.gateway.clientId}:${Constants.ROLE_KEY}${Constants.KEYS_SUFFIX}` : `${Constants.ROLE_KEY}${Constants.KEYS_SUFFIX}`, `${payload.data.d.guild_id}:${payload.data.d.role_id}`);
+        await this.container.gateway.cache.roles.delete(`${payload.data.d.guild_id}:${payload.data.d.role_id}`);
         this.container.gateway.amqp.sender.publish(this.container.gateway.clientId, payload.data.t, { ...payload }, { persistent: false });
     }
 }
