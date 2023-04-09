fb32a456c3a0d87b7f3b2ac6dbe019f7acb0a7f4
kagchi <yonuxbot@gmail.com>
2023-04-09 10:05:57 +0700

    feat: use shared constants

--- src/Utilities/Constants.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable @typescript-eslint/no-extraneous-class */
-export class Constants {
-    public static readonly PROMETHEUS_TASK = "prometheus-task";
-    public static readonly SPAWN_NEW_SHARD_TASK = "spawn-new-shard-task";
-    public static readonly STATISTICS_TASK = "statistics-task";
-}
