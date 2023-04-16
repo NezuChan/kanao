bde4c21e3862e6b1843ec1f660b67c36e94cd6fd
kagchi <yonuxbot@gmail.com>
2023-04-16 14:00:39 +0700

    feat: restart worker if there any error

--- src/Utilities/WebSockets/ProcessShardingStrategy.ts
+++ src/Utilities/WebSockets/ProcessShardingStrategy.ts
@@ -159,9 +159,15 @@ export class ProcessShardingStrategy implements IShardingStrategy {
         worker
             .on("error", error => {
                 this.manager.emit(WebSocketShardEvents.Error, { error, shardId: workerData.shardIds[0] });
+                worker.removeAllListeners();
+                this.#workers.splice(this.#workers.indexOf(worker), 1);
+                this.restartWorker(workerData);
             })
             .on("messageerror", error => {
                 this.manager.emit(WebSocketShardEvents.Error, { error, shardId: workerData.shardIds[0] });
+                worker.removeAllListeners();
+                this.#workers.splice(this.#workers.indexOf(worker), 1);
+                this.restartWorker(workerData);
             })
             .on("close", (code, signal) => {
                 this.manager.emit(WebSocketShardEvents.Error, { error: new Error(`Process closed with code ${code ?? "-"} and signal ${signal ?? "-"}. attempting to restart`), shardId: workerData.shardIds[0] });
