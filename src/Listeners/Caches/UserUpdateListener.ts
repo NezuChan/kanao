826dc45e7bc1386b68803a5ea092935ec8f3ffd5
kagchi <yonuxbot@gmail.com>
2023-04-17 20:10:40 +0700

    fix: listen for user update

---
+++ src/Listeners/Caches/UserUpdateListener.ts
@@ -0,0 +1,18 @@
+import { Listener, ListenerContext } from "../../Stores/Listener.js";
+import { GatewayDispatchEvents, GatewayUserUpdateDispatch } from "discord-api-types/v10";
+import { stateMessages } from "../../config.js";
+import { RedisKey } from "@nezuchan/constants";
+import { GenKey } from "../../Utilities/GenKey.js";
+
+export class UserUpdateListener extends Listener {
+    public constructor(context: ListenerContext) {
+        super(context, {
+            event: GatewayDispatchEvents.UserUpdate,
+            enabled: stateMessages
+        });
+    }
+
+    public async run(payload: { data: GatewayUserUpdateDispatch }): Promise<void> {
+        await this.store.redis.set(GenKey(RedisKey.BOT_USER_KEY), JSON.stringify(payload.data.d));
+    }
+}
