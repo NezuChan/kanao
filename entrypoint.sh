1b149ded759312d6aeded237889c3fae459ddf4d
kagchi <yonuxbot@gmail.com>
2023-08-04 14:51:38 +0700

    chore: update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 set -e
 
 # Example Environment Variables (replace with your own)
@@ -19,27 +20,22 @@ if [[ -f "/tmp/shard_id_start" ]] && [[ -f "/tmp/shard_id_end" ]] && [[ -f "/tmp
     else
         SHARD_START=$((TEMP_GATEWAY_SHARD_START + TEMP_GATEWAY_SHARD_END + 1)) # 0 + 5 + 1 = 6 (Example calculation)
         echo "${SHARD_START}" > /tmp/shard_id_start 
-        export GATEWAY_SHARD_START=$(cat /tmp/shard_id_start)
 
         SHARD_END=$((TEMP_GATEWAY_SHARD_END + GATEWAY_SHARD_COUNT_PER_REPLICA)) # 5 + 6 = 11 (Example calculation)
         echo "${SHARD_END}" > /tmp/shard_id_end
-        export GATEWAY_SHARD_END=$(cat /tmp/shard_id_end)
 
         REPLICA=$((TEMP_REPLICA_ID + 1)) # 0 + 1 = 1 (Example calculation)
         echo "${REPLICA}" > /tmp/replica_id
-        export REPLICA_ID=$(cat /tmp/replica_id)
+
+        echo "[ENTRYPOINT] INFO: Set shard ID to: $SHARD_START & Set end shard ID to: $SHARD_END, REPLICA ID $REPLICA"
     fi
 else
     echo ${GATEWAY_SHARD_START:=0} > /tmp/shard_id_start
-    export GATEWAY_SHARD_START=$(cat /tmp/shard_id_start)
 
     SHARD_END=$((GATEWAY_SHARD_COUNT_PER_REPLICA - 1))
     echo "${SHARD_END}" > /tmp/shard_id_end
-    export GATEWAY_SHARD_END=$(cat /tmp/shard_id_end)
 
     echo ${REPLICA_ID:=0} > /tmp/replica_id
-    export REPLICA_ID=$(cat /tmp/replica_id)
-
     echo "[ENTRYPOINT] INFO: Set initial shard ID to: $GATEWAY_SHARD_START & Set end shard ID to: $GATEWAY_SHARD_END, REPLICA ID $REPLICA_ID"
 fi
 
@@ -52,8 +48,8 @@ function cleanup() {
 
 trap "cleanup" SIGKILL SIGTERM SIGHUP SIGINT
 
-echo "[ENTRYPOINT] Sleeping for 3s" && sleep 3
-
-node -r dotenv/config dist/index.js
+echo "[ENTRYPOINT] Sleeping for 5s" && sleep 5
+echo "[ENTRYPOINT] Starting shard ID: $GATEWAY_SHARD_START & Ending shard ID: $GATEWAY_SHARD_END, REPLICA ID $REPLICA_ID, SHARD_COUNT $SHARD_COUNT"
+GATEWAY_SHARD_END=$(cat /tmp/shard_id_end) GATEWAY_SHARD_END=$(cat /tmp/shard_id_end) GATEWAY_SHARD_START=$(cat /tmp/shard_id_start) node -r dotenv/config dist/index.js
 child=$!
 wait "$child"
