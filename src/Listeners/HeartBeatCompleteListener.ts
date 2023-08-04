fe7ec4c7d6693627003e296ffc525827d06a3dfb
kagchi <yonuxbot@gmail.com>
2023-08-04 18:59:52 +0700

    fix: default status to empty object

--- src/Listeners/HeartBeatCompleteListener.ts
+++ src/Listeners/HeartBeatCompleteListener.ts
@@ -11,7 +11,7 @@ export class ReadyListener extends Listener {
 
     public async run(payload: { shard: WebSocketShard; shardId: number; data: { latency: number } }): Promise<void> {
         this.store.logger.debug(payload.data, `Shard ${payload.shardId} heartbeat complete`);
-        const status = JSON.parse((await this.store.redis.get(`${clientId}:gateway_shard_status:${payload.shardId}`))!) as { latency: number; status: number; startAt: number } | null;
+        const status = JSON.parse(await this.store.redis.get(`${clientId}:gateway_shard_status:${payload.shardId}`) ?? "{}") as object | { latency: number; status: number; startAt: number };
         await this.store.redis.set(`${clientId}:gateway_shard_status:${payload.shardId}`, JSON.stringify({ ...status, latency: payload.data.latency, lastAck: Date.now(), status: payload.shard.status }));
     }
 }
