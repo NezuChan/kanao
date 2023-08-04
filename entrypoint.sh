9d9c6a2c9deed172a7aabfefe784e5ad80e239a6
kagchi <yonuxbot@gmail.com>
2023-08-04 14:11:53 +0700

    feat: export env

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,26 +20,26 @@ if [[ -f "/tmp/shard_id_start" ]] && [[ -f "/tmp/shard_id_end" ]] && [[ -f "/tmp
     else
         SHARD_START=$((GATEWAY_SHARD_START + GATEWAY_SHARD_END + 1)) # 0 + 5 + 1 = 6 (Example calculation)
         echo "${SHARD_START}" > /tmp/shard_id_start 
-        GATEWAY_SHARD_START=$(cat /tmp/shard_id_start)
+        export GATEWAY_SHARD_START=$(cat /tmp/shard_id_start)
 
         SHARD_END=$((GATEWAY_SHARD_END + GATEWAY_SHARD_COUNT_PER_REPLICA)) # 5 + 6 = 11 (Example calculation)
         echo "${SHARD_END}" > /tmp/shard_id_end
-        GATEWAY_SHARD_END=$(cat /tmp/shard_id_end)
+        export GATEWAY_SHARD_END=$(cat /tmp/shard_id_end)
 
         REPLICA=$((REPLICA_ID + 1)) # 0 + 1 = 1 (Example calculation)
         echo "${REPLICA}" > /tmp/replica_id
-        REPLICA_ID=$(cat /tmp/replica_id)
+        export REPLICA_ID=$(cat /tmp/replica_id)
     fi
 else
     echo ${GATEWAY_SHARD_START:=0} > /tmp/shard_id_start
-    GATEWAY_SHARD_START=$(cat /tmp/shard_id_start)
+    export GATEWAY_SHARD_START=$(cat /tmp/shard_id_start)
 
     SHARD_END=$((GATEWAY_SHARD_COUNT_PER_REPLICA - 1))
     echo "${SHARD_END}" > /tmp/shard_id_end
-    GATEWAY_SHARD_END=$(cat /tmp/shard_id_end)
+    export GATEWAY_SHARD_END=$(cat /tmp/shard_id_end)
 
     echo ${REPLICA_ID:=0} > /tmp/replica_id
-    REPLICA_ID=$(cat /tmp/replica_id)
+    export REPLICA_ID=$(cat /tmp/replica_id)
 
     echo "[ENTRYPOINT] INFO: Set initial shard ID to: $GATEWAY_SHARD_START & Set end shard ID to: $GATEWAY_SHARD_END, REPLICA ID $REPLICA_ID"
 fi
