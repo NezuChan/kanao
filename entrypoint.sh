784c6fdab884c31e1ff68251fd125b8d7dcabf8b
kagchi <yonuxbot@gmail.com>
2023-08-04 14:42:39 +0700

    chore: update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 set -e
 
 # Example Environment Variables (replace with your own)
@@ -10,23 +9,23 @@ set -e
 
 
 if [[ -f "/tmp/shard_id_start" ]] && [[ -f "/tmp/shard_id_end" ]] && [[ -f "/tmp/replica_id" ]]; then
-    GATEWAY_SHARD_START=$(cat /tmp/shard_id_start)
-    GATEWAY_SHARD_END=$(cat /tmp/shard_id_end)
-    REPLICA_ID=$(cat /tmp/replica_id)
+    TEMP_GATEWAY_SHARD_START=$(cat /tmp/shard_id_start)
+    TEMP_GATEWAY_SHARD_END=$(cat /tmp/shard_id_end)
+    TEMP_REPLICA_ID=$(cat /tmp/replica_id)
 
-    if [[ $REPLICA_ID -gt $REPLICA_COUNT ]]; then
+    if [[ $TEMP_REPLICA_ID -gt $REPLICA_COUNT ]]; then
         echo "[ENTRYPOINT] ERROR: Max replica ID exceeded (${REPLICA_ID})."
         exit 1
     else
-        SHARD_START=$((GATEWAY_SHARD_START + GATEWAY_SHARD_END + 1)) # 0 + 5 + 1 = 6 (Example calculation)
+        SHARD_START=$((TEMP_GATEWAY_SHARD_START + TEMP_GATEWAY_SHARD_END + 1)) # 0 + 5 + 1 = 6 (Example calculation)
         echo "${SHARD_START}" > /tmp/shard_id_start 
         export GATEWAY_SHARD_START=$(cat /tmp/shard_id_start)
 
-        SHARD_END=$((GATEWAY_SHARD_END + GATEWAY_SHARD_COUNT_PER_REPLICA)) # 5 + 6 = 11 (Example calculation)
+        SHARD_END=$((TEMP_GATEWAY_SHARD_END + GATEWAY_SHARD_COUNT_PER_REPLICA)) # 5 + 6 = 11 (Example calculation)
         echo "${SHARD_END}" > /tmp/shard_id_end
         export GATEWAY_SHARD_END=$(cat /tmp/shard_id_end)
 
-        REPLICA=$((REPLICA_ID + 1)) # 0 + 1 = 1 (Example calculation)
+        REPLICA=$((TEMP_REPLICA_ID + 1)) # 0 + 1 = 1 (Example calculation)
         echo "${REPLICA}" > /tmp/replica_id
         export REPLICA_ID=$(cat /tmp/replica_id)
     fi
@@ -45,9 +44,7 @@ else
 fi
 
 function cleanup() {
-    rm -f /tmp/shard_id_start
-    rm -f /tmp/shard_id_end
-    rm -f /tmp/replica_id
+    rm -f /tmp/shard_id_start /tmp/shard_id_end /tmp/replica_id
     echo "[ENTRYPOINT] INFO: Shutting down..."
     kill -TERM "$child"
     wait "$child"
