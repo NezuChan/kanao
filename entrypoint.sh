cb9a3d30cd0bcd0838c95dbed84469f0d40e0b8b
kagchi <yonuxbot@gmail.com>
2023-08-04 14:26:59 +0700

    chore: update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,13 @@ set -e
 # GATEWAY_SHARD_COUNT = 18
 # GATEWAY_SHARD_COUNT_PER_REPLICA = 6
 
-do_math() {
-  awk "BEGIN { print $* }"
-}
 
 if [[ -f "/tmp/shard_id_start" ]] && [[ -f "/tmp/shard_id_end" ]] && [[ -f "/tmp/replica_id" ]]; then
     GATEWAY_SHARD_START=$(cat /tmp/shard_id_start)
     GATEWAY_SHARD_END=$(cat /tmp/shard_id_end)
     REPLICA_ID=$(cat /tmp/replica_id)
 
-    MAX_REPLICA=$(do_math "${REPLICA_COUNT:-1} - 1")
-    if [[ $REPLICA_ID -gt $MAX_REPLICA ]]; then
+    if [[ $REPLICA_ID -gt $REPLICA_COUNT ]]; then
         echo "[ENTRYPOINT] ERROR: Max replica ID exceeded (${REPLICA_ID})."
         exit 1
     else
