c7f6a6a684924dc2ef67fe055591d5b1272b6d4a
kagchi <yonuxbot@gmail.com>
2023-09-09 17:17:57 +0700

    chore: properly handle presence update data

--- src/Listeners/Caches/Guilds/PresenceUpdateListener.ts
+++ src/Listeners/Caches/Guilds/PresenceUpdateListener.ts
@@ -1,21 +1,21 @@
 import { Listener, ListenerContext } from "../../../Stores/Listener.js";
-import { GatewayDispatchEvents, GatewayPresenceUpdate } from "discord-api-types/v10";
+import { GatewayDispatchEvents, GatewayPresenceUpdateDispatch } from "discord-api-types/v10";
 import { clientId, statePresences } from "../../../config.js";
 import { RabbitMQ, RedisKey } from "@nezuchan/constants";
 import { GenKey } from "../../../Utilities/GenKey.js";
 import { RoutingKey } from "@nezuchan/utilities";
 
-export class GuildMemberUpdateListener extends Listener {
+export class PresenceUpdateListener extends Listener {
     public constructor(context: ListenerContext) {
         super(context, {
             event: GatewayDispatchEvents.PresenceUpdate
         });
     }
 
-    public async run(payload: { data: GatewayPresenceUpdate; shardId: number }): Promise<void> {
+    public async run(payload: { data: GatewayPresenceUpdateDispatch; shardId: number }): Promise<void> {
         if (statePresences) {
-            await this.store.redis.set(GenKey(`${RedisKey.PRESENCE_KEY}`, payload.data.user.id, payload.data.guild_id), JSON.stringify(payload.data));
-            await this.store.redis.sadd(GenKey(`${RedisKey.PRESENCE_KEY}${RedisKey.KEYS_SUFFIX}`, payload.data.guild_id), GenKey(`${RedisKey.PRESENCE_KEY}`, payload.data.user.id, payload.data.guild_id));
+            await this.store.redis.set(GenKey(`${RedisKey.PRESENCE_KEY}`, payload.data.d.user.id, payload.data.d.guild_id), JSON.stringify(payload.data.d));
+            await this.store.redis.sadd(GenKey(`${RedisKey.PRESENCE_KEY}${RedisKey.KEYS_SUFFIX}`, payload.data.d.guild_id), GenKey(`${RedisKey.PRESENCE_KEY}`, payload.data.d.user.id, payload.data.d.guild_id));
         }
 
         await this.store.amqp.publish(RabbitMQ.GATEWAY_QUEUE_SEND, RoutingKey(clientId, payload.shardId), Buffer.from(JSON.stringify(payload.data)));
