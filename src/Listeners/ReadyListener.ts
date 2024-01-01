bb3f4d868fbfc5a063d0f71191a6aebd382f3cde
Hazmi Alfarizqi <contact@hzmi.xyz>
2024-01-01 22:36:32 +0700

    feat: update counters after connect, not after shard 0 is ready

--- src/Listeners/ReadyListener.ts
+++ src/Listeners/ReadyListener.ts
@@ -3,8 +3,6 @@ import { Listener, ListenerContext } from "../Stores/Listener.js";
 import { GatewayReadyDispatch } from "discord-api-types/v10";
 import { GenKey } from "../Utilities/GenKey.js";
 import { RedisKey } from "@nezuchan/constants";
-import { redisScan } from "@nezuchan/utilities";
-import { redisScanCount } from "../config.js";
 
 export class ReadyListener extends Listener {
     public constructor(context: ListenerContext) {
@@ -15,18 +13,6 @@ export class ReadyListener extends Listener {
 
     public async run(payload: { data: { data: GatewayReadyDispatch["d"] }; shardId: number }): Promise<unknown> {
         await this.store.redis.set(GenKey(RedisKey.BOT_USER_KEY), JSON.stringify(payload.data.data.user));
-
-        // Update counters
-        if (payload.shardId === 0) {
-            const guild = await redisScan(this.store.redis, GenKey(RedisKey.GUILD_KEY), redisScanCount);
-            await this.store.redis.set(GenKey(RedisKey.GUILD_KEY, RedisKey.COUNT), guild.length);
-
-            const channel = await redisScan(this.store.redis, GenKey(RedisKey.CHANNEL_KEY), redisScanCount);
-            await this.store.redis.set(GenKey(RedisKey.CHANNEL_KEY, RedisKey.COUNT), channel.length);
-
-            const user = await redisScan(this.store.redis, GenKey(RedisKey.USER_KEY), redisScanCount);
-            await this.store.redis.set(GenKey(RedisKey.USER_KEY, RedisKey.COUNT), user.length);
-        }
 
         return this.logger.info(`Shard ${payload.shardId} is ready !`);
     }
