57d520df5689a985ba030075bdee0d1086ed2a5a
KagChi <yonuxbot@gmail.com>
2024-02-12 20:28:04 +0700

    fix: remove unused field

--- src/Schema/collections/member.ts
+++ src/Schema/collections/member.ts
@@ -4,7 +4,7 @@ import { memberRoles } from "./roles.js";
 import { users } from "./user.js";
 
 export const members = pgTable("members", {
-    id: text("id").primaryKey(),
+    id: text("id").primaryKey().references(() => users.id, { onDelete: "cascade" }),
     nick: text("nick"),
     avatar: text("avatar"),
     flags: integer("flags"),
@@ -14,8 +14,7 @@ export const members = pgTable("members", {
     mute: boolean("mute"),
     pending: boolean("pending"),
     permissions: integer("permissions"),
-    communicationDisabledUntil: text("communication_disabled_until"),
-    userId: text("user_id").references(() => users.id, { onDelete: "cascade" })
+    communicationDisabledUntil: text("communication_disabled_until")
 });
 
 export const membersRelations = relations(members, ({ many }) => ({
