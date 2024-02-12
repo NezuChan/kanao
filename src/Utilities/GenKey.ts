6117ec8beafc8341e77649ed1822523cb882db71
Hazmi Alfarizqi <contact@hzmi.xyz>
2024-02-13 00:18:03 +0700

    fix: remove GenKey

--- src/Utilities/GenKey.ts
+++
@@ -1,6 +0,0 @@
-import { GenKey as OGenKey } from "@nezuchan/utilities";
-import { clientId } from "../config.js";
-
-export function GenKey(prefix: string, key?: string, guildId?: string): string {
-    return OGenKey(clientId, prefix, key, guildId);
-}
