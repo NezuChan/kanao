a59324c20dd8e44419b9233cfb208d6a34092220
hzmi <contact@hzmi.xyz>
2024-02-21 07:10:48 +0000

    fix(GuildCreateListener): don't store ops, execute them directly
    Co-authored-by: KagChi <KagChi@users.noreply.github.com>
    Co-authored-by: Vann <stevanvincent05@gmail.com>

--- services/kanao-gateway/src/Listeners/Caches/Guilds/GuildCreateListener.ts
+++ services/kanao-gateway/src/Listeners/Caches/Guilds/GuildCreateListener.ts
@@ -4,7 +4,6 @@ import { guilds, memberRoles, members, users, voiceStates } from "@nezuchan/kana
 import { RoutingKey } from "@nezuchan/utilities";
 import type { GatewayGuildCreateDispatch } from "discord-api-types/v10";
 import { GatewayDispatchEvents } from "discord-api-types/v10";
-import type { SQL } from "drizzle-orm";
 import { sql } from "drizzle-orm";
 import type { ListenerContext } from "../../../Stores/Listener.js";
 import { Listener } from "../../../Stores/Listener.js";
@@ -38,13 +37,50 @@ export class GuildCreateListener extends Listener {
         // @ts-expect-error deallocate value
         payload.data.d.threads = null;
 
-        let ops: SQL[] = [];
-
-        ops.push(
-            this.store.drizzle
-                .insert(guilds)
-                .values({
-                    id: payload.data.d.id,
+        await this.store.drizzle
+            .insert(guilds)
+            .values({
+                id: payload.data.d.id,
+                unavailable: payload.data.d.unavailable,
+                name: payload.data.d.name,
+                banner: payload.data.d.banner,
+                owner: payload.data.d.owner,
+                ownerId: payload.data.d.owner_id,
+                afkChannelId: payload.data.d.afk_channel_id,
+                afkTimeout: payload.data.d.afk_timeout,
+                defaultMessageNotifications: payload.data.d.default_message_notifications,
+                explicitContentFilter: payload.data.d.explicit_content_filter,
+                icon: payload.data.d.icon,
+                mfaLevel: payload.data.d.mfa_level,
+                region: payload.data.d.region,
+                systemChannelId: payload.data.d.system_channel_id,
+                verificationLevel: payload.data.d.verification_level,
+                widgetChannelId: payload.data.d.widget_channel_id,
+                widgetEnabled: payload.data.d.widget_enabled,
+                approximateMemberCount: payload.data.d.approximate_member_count,
+                approximatePresenceCount: payload.data.d.approximate_presence_count,
+                description: payload.data.d.description,
+                discoverySplash: payload.data.d.discovery_splash,
+                iconHash: payload.data.d.icon_hash,
+                maxMembers: payload.data.d.max_members,
+                maxPresences: payload.data.d.max_presences,
+                premiumSubscriptionCount: payload.data.d.premium_subscription_count,
+                premiumTier: payload.data.d.premium_tier,
+                vanityUrlCode: payload.data.d.vanity_url_code,
+                nsfwLevel: payload.data.d.nsfw_level,
+                rulesChannelId: payload.data.d.rules_channel_id,
+                publicUpdatesChannelId: payload.data.d.public_updates_channel_id,
+                preferredLocale: payload.data.d.preferred_locale,
+                maxVideoChannelUsers: payload.data.d.max_video_channel_users,
+                permissions: payload.data.d.permissions,
+                premiumProgressBarEnabled: payload.data.d.premium_progress_bar_enabled,
+                safetyAlertChannelId: payload.data.d.safety_alerts_channel_id,
+                splash: payload.data.d.splash,
+                systemChannelFlags: payload.data.d.system_channel_flags
+            })
+            .onConflictDoUpdate({
+                target: guilds.id,
+                set: {
                     unavailable: payload.data.d.unavailable,
                     name: payload.data.d.name,
                     banner: payload.data.d.banner,
@@ -81,50 +117,8 @@ export class GuildCreateListener extends Listener {
                     safetyAlertChannelId: payload.data.d.safety_alerts_channel_id,
                     splash: payload.data.d.splash,
                     systemChannelFlags: payload.data.d.system_channel_flags
-                })
-                .onConflictDoUpdate({
-                    target: guilds.id,
-                    set: {
-                        unavailable: payload.data.d.unavailable,
-                        name: payload.data.d.name,
-                        banner: payload.data.d.banner,
-                        owner: payload.data.d.owner,
-                        ownerId: payload.data.d.owner_id,
-                        afkChannelId: payload.data.d.afk_channel_id,
-                        afkTimeout: payload.data.d.afk_timeout,
-                        defaultMessageNotifications: payload.data.d.default_message_notifications,
-                        explicitContentFilter: payload.data.d.explicit_content_filter,
-                        icon: payload.data.d.icon,
-                        mfaLevel: payload.data.d.mfa_level,
-                        region: payload.data.d.region,
-                        systemChannelId: payload.data.d.system_channel_id,
-                        verificationLevel: payload.data.d.verification_level,
-                        widgetChannelId: payload.data.d.widget_channel_id,
-                        widgetEnabled: payload.data.d.widget_enabled,
-                        approximateMemberCount: payload.data.d.approximate_member_count,
-                        approximatePresenceCount: payload.data.d.approximate_presence_count,
-                        description: payload.data.d.description,
-                        discoverySplash: payload.data.d.discovery_splash,
-                        iconHash: payload.data.d.icon_hash,
-                        maxMembers: payload.data.d.max_members,
-                        maxPresences: payload.data.d.max_presences,
-                        premiumSubscriptionCount: payload.data.d.premium_subscription_count,
-                        premiumTier: payload.data.d.premium_tier,
-                        vanityUrlCode: payload.data.d.vanity_url_code,
-                        nsfwLevel: payload.data.d.nsfw_level,
-                        rulesChannelId: payload.data.d.rules_channel_id,
-                        publicUpdatesChannelId: payload.data.d.public_updates_channel_id,
-                        preferredLocale: payload.data.d.preferred_locale,
-                        maxVideoChannelUsers: payload.data.d.max_video_channel_users,
-                        permissions: payload.data.d.permissions,
-                        premiumProgressBarEnabled: payload.data.d.premium_progress_bar_enabled,
-                        safetyAlertChannelId: payload.data.d.safety_alerts_channel_id,
-                        splash: payload.data.d.splash,
-                        systemChannelFlags: payload.data.d.system_channel_flags
-                    }
-                })
-                .getSQL()
-        );
+                }
+            });
 
         if (stateRoles) {
             const values = sql.empty();
@@ -133,52 +127,63 @@ export class GuildCreateListener extends Listener {
                 values.append(sql`${values.queryChunks.length === 0 ? undefined : sql.raw(", ")}(${role.id}, ${role.name}, ${role.permissions}, ${role.position}, ${role.color}, ${role.hoist}, ${payload.data.d.id})`);
             }
 
-            ops.push(
+            await this.store.drizzle.execute(
                 sql.join([
                     sql`INSERT INTO roles (id, name, permissions, position, color, hoist, guild_id) VALUES `,
                     values,
                     sql` ON CONFLICT (id) DO UPDATE SET name = EXCLUDED.name, permissions = EXCLUDED.permissions, position = EXCLUDED.position, color = EXCLUDED.color, hoist = EXCLUDED.hoist`
                 ])
             );
-
-            // @ts-expect-error deallocate array
-            payload.data.d.roles = null;
         }
+
+        // @ts-expect-error deallocate array
+        // eslint-disable-next-line require-atomic-updates
+        payload.data.d.roles = null;
 
         const bot = payload.data.d.members.find(member => member.user?.id === clientId)!;
 
-        ops.push(
-            this.store.drizzle
-                .insert(users)
-                .values({
-                    id: bot.user!.id,
-                    username: bot.user!.username,
-                    discriminator: bot.user!.discriminator ?? null,
-                    globalName: bot.user!.global_name ?? null,
-                    avatar: bot.user!.avatar ?? null,
-                    bot: bot.user!.bot ?? false,
-                    flags: bot.user!.flags,
-                    premiumType: bot.user!.premium_type,
-                    publicFlags: bot.user!.public_flags
-                })
-                .onConflictDoUpdate({
-                    target: users.id,
-                    set: {
-                        username: sql`EXCLUDED.username`,
-                        discriminator: sql`EXCLUDED.discriminator`,
-                        globalName: sql`EXCLUDED.global_name`,
-                        avatar: sql`EXCLUDED.avatar`,
-                        bot: sql`EXCLUDED.bot`,
-                        flags: sql`EXCLUDED.flags`,
-                        premiumType: sql`EXCLUDED.premium_type`,
-                        publicFlags: sql`EXCLUDED.public_flags`
-                    }
-                })
-                .getSQL(),
-            this.store.drizzle.insert(members)
-                .values({
-                    id: bot.user!.id,
-                    guildId: payload.data.d.id,
+        await this.store.drizzle
+            .insert(users)
+            .values({
+                id: bot.user!.id,
+                username: bot.user!.username,
+                discriminator: bot.user!.discriminator ?? null,
+                globalName: bot.user!.global_name ?? null,
+                avatar: bot.user!.avatar ?? null,
+                bot: bot.user!.bot ?? false,
+                flags: bot.user!.flags,
+                premiumType: bot.user!.premium_type,
+                publicFlags: bot.user!.public_flags
+            })
+            .onConflictDoUpdate({
+                target: users.id,
+                set: {
+                    username: sql`EXCLUDED.username`,
+                    discriminator: sql`EXCLUDED.discriminator`,
+                    globalName: sql`EXCLUDED.global_name`,
+                    avatar: sql`EXCLUDED.avatar`,
+                    bot: sql`EXCLUDED.bot`,
+                    flags: sql`EXCLUDED.flags`,
+                    premiumType: sql`EXCLUDED.premium_type`,
+                    publicFlags: sql`EXCLUDED.public_flags`
+                }
+            });
+        await this.store.drizzle.insert(members)
+            .values({
+                id: bot.user!.id,
+                guildId: payload.data.d.id,
+                avatar: bot.avatar,
+                flags: bot.flags,
+                communicationDisabledUntil: bot.communication_disabled_until,
+                deaf: bot.deaf,
+                joinedAt: bot.joined_at,
+                mute: bot.mute,
+                nick: bot.nick,
+                pending: bot.pending,
+                premiumSince: bot.premium_since
+            }).onConflictDoUpdate({
+                target: members.id,
+                set: {
                     avatar: bot.avatar,
                     flags: bot.flags,
                     communicationDisabledUntil: bot.communication_disabled_until,
@@ -188,40 +193,25 @@ export class GuildCreateListener extends Listener {
                     nick: bot.nick,
                     pending: bot.pending,
                     premiumSince: bot.premium_since
-                }).onConflictDoUpdate({
-                    target: members.id,
-                    set: {
-                        avatar: bot.avatar,
-                        flags: bot.flags,
-                        communicationDisabledUntil: bot.communication_disabled_until,
-                        deaf: bot.deaf,
-                        joinedAt: bot.joined_at,
-                        mute: bot.mute,
-                        nick: bot.nick,
-                        pending: bot.pending,
-                        premiumSince: bot.premium_since
-                    }
-                })
-                .getSQL()
-        );
+                }
+            });
 
         if (bot.roles.length > 0) {
-            ops.push(
-                this.store.drizzle.insert(memberRoles)
-                    .values(bot.roles.map(role => ({
-                        memberId: bot.user!.id,
-                        roleId: role,
-                        guildId: payload.data.d.id
-                    })).filter(role => role.roleId !== null))
-                    .onConflictDoNothing({ target: [memberRoles.memberId, memberRoles.roleId] })
-                    .getSQL()
-            );
-
-            // @ts-expect-error deallocate array
-            bot.roles = null;
+            await this.store.drizzle.insert(memberRoles)
+                .values(bot.roles.map(role => ({
+                    memberId: bot.user!.id,
+                    roleId: role,
+                    guildId: payload.data.d.id
+                })).filter(role => role.roleId !== null))
+                .onConflictDoNothing({ target: [memberRoles.memberId, memberRoles.roleId] });
         }
 
         // @ts-expect-error deallocate array
+        // eslint-disable-next-line require-atomic-updates
+        bot.roles = null;
+
+        // @ts-expect-error deallocate array
+        // eslint-disable-next-line require-atomic-updates
         payload.data.d.members = null;
 
         if (stateChannels && payload.data.d.channels.length > 0) {
@@ -242,7 +232,7 @@ export class GuildCreateListener extends Listener {
                 }
             }
 
-            ops.push(
+            await this.store.drizzle.execute(
                 sql.join([
                     sql`INSERT INTO channels (id, guild_id, name, type, flags) VALUES `,
                     values,
@@ -251,7 +241,7 @@ export class GuildCreateListener extends Listener {
             );
 
             if (values2.queryChunks.length > 0) {
-                ops.push(
+                await this.store.drizzle.execute(
                     sql.join([
                         sql`INSERT INTO channels_overwrite (user_or_role, channel_id, type, allow, deny) VALUES `,
                         values2,
@@ -262,67 +252,48 @@ export class GuildCreateListener extends Listener {
         }
 
         // @ts-expect-error deallocate array
+        // eslint-disable-next-line require-atomic-updates
         payload.data.d.channels = null;
 
         const voiceState = payload.data.d.voice_states.find(voice => voice.user_id === clientId && voice.channel_id !== undefined);
 
         if (voiceState) {
-            ops.push(
-                this.store.drizzle
-                    .insert(voiceStates)
-                    .values({
-                        memberId: voiceState.user_id,
-                        guildId: payload.data.d.id,
-                        channelId: voiceState.channel_id!,
-                        sessionId: voiceState.session_id,
-                        deaf: voiceState.deaf,
-                        mute: voiceState.mute,
-                        requestToSpeakTimestamp: voiceState.request_to_speak_timestamp,
-                        selfDeaf: voiceState.self_deaf,
-                        selfMute: voiceState.self_mute,
-                        selfStream: voiceState.self_stream,
-                        selfVideo: voiceState.self_video,
-                        suppress: voiceState.suppress
-                    })
-                    .onConflictDoUpdate({
-                        target: [voiceStates.memberId, voiceStates.guildId],
-                        set: {
-                            channelId: sql`EXCLUDED.channel_id`,
-                            sessionId: sql`EXCLUDED.session_id`,
-                            deaf: sql`EXCLUDED.deaf`,
-                            mute: sql`EXCLUDED.mute`,
-                            requestToSpeakTimestamp: sql`EXCLUDED.request_to_speak_timestamp`,
-                            selfDeaf: sql`EXCLUDED.self_deaf`,
-                            selfMute: sql`EXCLUDED.self_mute`,
-                            selfStream: sql`EXCLUDED.self_stream`,
-                            selfVideo: sql`EXCLUDED.self_video`,
-                            suppress: sql`EXCLUDED.suppress`
-                        }
-                    })
-                    .getSQL()
-            );
+            await this.store.drizzle
+                .insert(voiceStates)
+                .values({
+                    memberId: voiceState.user_id,
+                    guildId: payload.data.d.id,
+                    channelId: voiceState.channel_id!,
+                    sessionId: voiceState.session_id,
+                    deaf: voiceState.deaf,
+                    mute: voiceState.mute,
+                    requestToSpeakTimestamp: voiceState.request_to_speak_timestamp,
+                    selfDeaf: voiceState.self_deaf,
+                    selfMute: voiceState.self_mute,
+                    selfStream: voiceState.self_stream,
+                    selfVideo: voiceState.self_video,
+                    suppress: voiceState.suppress
+                })
+                .onConflictDoUpdate({
+                    target: [voiceStates.memberId, voiceStates.guildId],
+                    set: {
+                        channelId: sql`EXCLUDED.channel_id`,
+                        sessionId: sql`EXCLUDED.session_id`,
+                        deaf: sql`EXCLUDED.deaf`,
+                        mute: sql`EXCLUDED.mute`,
+                        requestToSpeakTimestamp: sql`EXCLUDED.request_to_speak_timestamp`,
+                        selfDeaf: sql`EXCLUDED.self_deaf`,
+                        selfMute: sql`EXCLUDED.self_mute`,
+                        selfStream: sql`EXCLUDED.self_stream`,
+                        selfVideo: sql`EXCLUDED.self_video`,
+                        suppress: sql`EXCLUDED.suppress`
+                    }
+                });
         }
 
         // @ts-expect-error deallocate array
+        // eslint-disable-next-line require-atomic-updates
         payload.data.d.voice_states = null;
-
-        for (const op of ops) {
-            try {
-                await this.store.drizzle.execute(op);
-            } catch (error) {
-                this.logger.error(error, op.toQuery({
-                    escapeName: String,
-                    escapeString: String,
-                    escapeParam: (_, v) => typeof v === "string" ? `'${v}'` : v as string
-                }).sql);
-            }
-
-            // @ts-expect-error deallocate value
-            ops[ops.indexOf(op)] = null;
-        }
-
-        // @ts-expect-error deallocate array
-        ops = null;
 
         await this.store.amqp.publish(
             RabbitMQ.GATEWAY_QUEUE_SEND,
