df77eff98e6b902fbbf58c5a7a6f29309d14934f
KagChi <yonuxbot@gmail.com>
2024-02-23 12:06:17 +0700

    chore: debug message

--- services/kanao-cache/src/Structures/KanaoCache.ts
+++ services/kanao-cache/src/Structures/KanaoCache.ts
@@ -44,6 +44,7 @@ export class KanaoCache extends EventEmitter {
         this.logger.info("Successfully bind to cache queue!");
 
         await channel.consume(queue, message => {
+            console.log(JSON.parse(message!.content.toString()));
             if (message && message.properties.replyTo === clientId) {
                 channel.ack(message);
                 this.emit("dispatch", JSON.parse(message.content.toString()));
